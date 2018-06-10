123e2516d8cc35811d7277a8bc1a07c160436ed7
Max Hatfull <max.hatfull@btinternet.com>
2018-06-10 09:06:38 +0100

    sign upload urls

--- handler.js
+++ handler.js
@@ -1,16 +1,25 @@
 'use strict';
 
+var AWS = require('aws-sdk');
+
 module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+  var s3 = new AWS.S3();
+  console.log(event.body);
+  var params = JSON.parse(event.body);
+  var s3Params = {
+    Bucket: 'stories.underlunchers.co.uk',
+    Key:  params.name,
+    ContentType: 'application/octet-stream',
+    ACL: 'public-read',
   };
 
-  callback(null, response);
+  var uploadURL = s3.getSignedUrl('putObject', s3Params);
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
+  callback(null, {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': 'https://game.underlunchers.co.uk'
+    },
+    body: JSON.stringify({ uploadURL: uploadURL }),
+  })
+}
